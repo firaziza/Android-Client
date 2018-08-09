288c569c53c032c6171090c3cc1e1c126d984554
Arif Aziz <arif.aziz@temple.edu>
2018-08-09 03:38:05 -0400

    Initial commit

---
+++ app/src/main/java/com/androidsrc/client/MainActivity.java
@@ -0,0 +1,53 @@
+package com.androidsrc.client;
+
+import android.os.Bundle;
+import android.app.Activity;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import java.sql.Time;
+
+public class MainActivity extends Activity {
+
+	TextView response;
+	EditText editTextAddress, editTextPort;
+	Button buttonConnect;
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
+
+		editTextAddress = (EditText) findViewById(R.id.addressEditText);
+		editTextPort = (EditText) findViewById(R.id.portEditText);
+		buttonConnect = (Button) findViewById(R.id.connectButton);
+		response = (TextView) findViewById(R.id.responseTextView);
+		buttonConnect.setOnClickListener(new OnClickListener() {
+
+			@Override
+			public void onClick(View arg0) {
+
+
+
+for (int i=0; i<1000;i++) {
+	Client myClient = new Client(editTextAddress.getText()
+			.toString(), Integer.parseInt(editTextPort
+			.getText().toString()), response);
+	myClient.execute();
+
+	try {
+		Thread.sleep(10);
+	} catch (InterruptedException e) {
+		e.printStackTrace();
+	}
+
+
+}
+		}
+		});
+
+	}
+
+}
